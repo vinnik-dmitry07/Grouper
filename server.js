5f39ea199ef9f406a222cd9a2df19157b0220a39
Dmitry <vinnik.dmitry07@gmail.com>
2021-03-10 17:35:33 +0200

    Deploy

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/project-name'));
+app.use(express.static(__dirname + '/dist/Grouper'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', {root: 'dist/project-name'}
-  );
+  res.sendFile(path.join(__dirname + '/dist/Grouper/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
-
-console.log(`Running on port ${process.env.PORT || 8080}`)
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+const host = 'https://grouper-8team-api.herokuapp.com';
+
 export const environment = {
-  production: true
+  production: true,
+  user: {
+    signupURL: host + '/api/User/sign-up',
+    signinURL: host + '/api/User/sign-in',
+  }
 };
